b403c4710ae3c17992764453a60886b86efee254
Mr. Hub <elluvasun.allah@gmail.com>
2025-09-14 18:58:24 -0400

    new session with hydrogen and oxygen

---
+++ crypto.mjs
@@ -0,0 +1,15 @@
+// crypto.mjs
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
+const hasher = createHash('sha1');
+
+hasher.setEncoding('hex');
+// ensure you have a `package.json` file for this test!
+hasher.write(await readFile('package.json'));
+hasher.end();
+
+const fileHash = hasher.read();
+
+// run with `node crypto.mjs`
+console.log(`The SHA-1 hash of package.json is: ${fileHash}`);
