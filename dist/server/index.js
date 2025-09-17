806836840b6164c50dfb191ad6a58330da5be19b
Mr. Hub <elluvasun.allah@gmail.com>
2025-09-17 04:50:16 -0400

    chore: track built server bundle for Oxygen deploy

---
+++ dist/server/index.js
@@ -0,0 +1,3 @@
+import{createServer as n}from"node:http";const o=n((t,e)=>{e.writeHead(200,{"Content-Type":"text/plain"}),e.end(`Hello World!
+`)});o.listen(3e3,"127.0.0.1",()=>{console.log("Listening on 127.0.0.1:3000")});
+//# sourceMappingURL=index.js.map
